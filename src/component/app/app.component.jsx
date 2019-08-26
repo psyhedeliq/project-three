fb52093315a90e598f6dc4dc79c2513b282a8043
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-26 21:28:39 +0100

    create app component

---
+++ src/component/app/app.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProjectThree = () => {
+  return <h1>This is ProjectThree</h1>;
+};
+
+export default ProjectThree;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-const ProjectThree = () => {
-  return <h1>This is ProjectThree</h1>;
-};
+import ProjectThree from './component/app';
 
 const mainDiv = document.querySelector('#renderHere');
 

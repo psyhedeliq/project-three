d98cdc04ace3ed1e197a08faef7505549b61a03e
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-26 21:32:12 +0100

    create employee component

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Employee from '../employee';
 
 const ProjectThree = () => {
-  return <h1>This is ProjectThree</h1>;
+  return (
+    <div>
+      <h1>This is ProjectThree</h1>
+      <Employee />
+    </div>
+  );
 };
 
 export default ProjectThree;
---
+++ src/component/employee/employee.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Employee = () => {
+  return <h2>Employees</h2>;
+};
+
+export default Employee;

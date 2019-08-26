71d49bf6c65edcc582175aa75a38434585283317
psyhedeliq <filipcornelcristian@gmail.com>
2019-08-26 21:35:27 +0100

    create employee-details component

--- src/component/app/app.component.jsx
+++ src/component/app/app.component.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Employee from '../employee';
+import Details from '../employee-details';
 
 const ProjectThree = () => {
   return (
     <div>
       <h1>This is ProjectThree</h1>
       <Employee />
+      <Details />
     </div>
   );
 };
---
+++ src/component/employee-details/employee-details.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Details = () => {
+  return <h3>This are the employee details</h3>;
+};
+
+export default Details;
